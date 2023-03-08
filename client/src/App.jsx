6f101deec30af9eb3f96b28cc089e51bc79d9f0c
tuanpa.tpa <tuanpa.tpa@gmail.com>
2023-03-09 00:24:50 +0700

    Revert "Revert "compoments""
    This reverts commit f41ad57b6b7382cdb3150c392ab3af76b28e2954.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { Navbar,Footer, Welcome, Loader, Services, Transactions } from "./components"
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+        <div>
+          <Services />
+          <Transactions />
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
