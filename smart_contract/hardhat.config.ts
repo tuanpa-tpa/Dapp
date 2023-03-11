32e9b4942f78497b518abdd4076256e2b3f61396
tuanpa.tpa <tuanpa.tpa@gmail.com>
2023-03-11 18:53:49 +0700

    config vs BSC network

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config({path: __dirname + "/.env"});
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [ process.env.PRIV_KEY ]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
 };
 
 export default config;
--- smart_contract/scripts/deploy.ts
+++ smart_contract/scripts/deploy.ts
@@ -1,14 +1,16 @@
 import { ethers, hardhatArguments } from 'hardhat';
+import * as Config from './config';
 
 async function main() {
+    await Config.initConfig();
     const network = hardhatArguments.network ? hardhatArguments.network : 'dev';
     const [deployer] = await ethers.getSigners();
     console.log('deploy from address: ', deployer.address);
 
     const Transactions = await ethers.getContractFactory("Transactions");
     const transactions = await Transactions.deploy();
-    console.log('Floppy address: ', transactions.address);
-    
+    console.log('Transactions address: ', transactions.address);
+    await Config.updateConfig();
 }
 
 main().then(() => process.exit(0))
